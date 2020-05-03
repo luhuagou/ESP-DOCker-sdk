76039b169c2dfc1dbdbc98a8d9a0ed0505dd510a
luhuagou <94329617+luhuagou@users.noreply.github.com>
2020-05-03 12:19:21 +0000

    mac: validate mac_partition is within sector

---
+++ gatsby-config.js
@@ -0,0 +1,29 @@
+require("dotenv").config({
+  path: `.env`,
+});
+
+module.exports = {
+  siteMetadata: {
+    title: `A react next landing page`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        minify: false, // Breaks styles if not set to false
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `common`,
