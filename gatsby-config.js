15eafa8ccc26555ca569a20560b45c64761c38ac
luhuagou <94329617+luhuagou@users.noreply.github.com>
2020-05-04 09:09:54 +0000

    net: fix 'ip rule' iif/oif device rename

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,3 +27,41 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `common`,
+        path: `${__dirname}/src/common/src/assets/`,
+      },
+    },
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/common/src/data/`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Roboto`,
+            variants: [
+              `100`,
+              `100i`,
+              `300`,
+              `300i`,
+              `400`,
+              `
