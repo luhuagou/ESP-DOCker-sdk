433499ab5a9ceead323cd6f5ccd68179a8ec9cb7
luhuagou <94329617+luhuagou@users.noreply.github.com>
2020-05-11 10:56:22 +0000

    Bump to version 0.3.0

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,1 +1,42 @@
-const path = require(
+const path = require(`path`)
+const _ = require('lodash')
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  const blogPost = path.resolve(`./src/templates/blog-post.js`)
+  const blogList = path.resolve(`./src/templates/blog-list.js`)
+
+  return graphql(
+    `
+      {
+        allContentfulBlog (
+          sort: { fields: [date], order: DESC }
+          limit: 1000
+        ) {
+          edges {
+            node {
+              content {
+                content
+              }
+              title
+              tags {
+                tagName
+              }
+              date(formatString: "DD [<span>] MMMM [</span>]")
+              slug
+            }
+          }
+        }
+      }
+    `
+  ).then(result => {
+    if (result.errors) {
+      throw result.errors
+    }
+
+    // Create blog posts pages.
+    const posts = result.data.allContentfulBlog.edges
+
+    posts.forEach((post, index) => {
+      const previous = ind
