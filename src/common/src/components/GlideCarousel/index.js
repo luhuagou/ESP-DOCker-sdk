aa01e82e33f0f0330bbdb1cce439285f710bb09d
luhuagou <94329617+luhuagou@users.noreply.github.com>
2022-08-11 13:14:00 +0000

    drm/i915: Fix eDP blank screen after S3 resume on HP desktops

--- src/common/src/components/GlideCarousel/index.js
+++ src/common/src/components/GlideCarousel/index.js
@@ -22,4 +22,47 @@ const GlideCarousel = ({
   bullets,
   numberOfBullets,
   buttonWrapperStyle,
-  bulletWrapper
+  bulletWrapperStyle,
+  bulletButtonStyle,
+  carouselSelector,
+}) => {
+  // Add all classs to an array
+  const addAllClasses = ['glide'];
+
+  // className prop checking
+  if (className) {
+    addAllClasses.push(className);
+  }
+
+  // number of bullets loop
+  const totalBullets = [];
+  for (let i = 0; i < numberOfBullets; i++) {
+    totalBullets.push(i);
+  }
+
+  // Load glide
+  useEffect(() => {
+    const glide = new Glide(
+      carouselSelector ? `#${carouselSelector}` : '#glide',
+      {
+        ...options,
+      }
+    );
+    glide.mount();
+  });
+
+  return (
+    <GlideWrapper
+      className={addAllClasses.join(' ')}
+      id={carouselSelector || 'glide'}
+    >
+      <div className="glide__track" data-glide-el="track">
+        <ul className="glide__slides">{children}</ul>
+      </div>
+
+      {/** if controls prop true then show glide controls nav */}
+      {controls && (
+        <ButtonControlWrapper
+          className="glide__controls"
+          data-glide-el="controls"
+          {...buttonWrapperSt
