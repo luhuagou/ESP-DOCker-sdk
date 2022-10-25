9922b30c0d180df0c1e5710089ae3888d43e36d7
luhuagou <94329617+luhuagou@users.noreply.github.com>
2022-10-25 17:18:12 +0000

    [MOD] version

--- src/common/src/theme/hosting/colors.js
+++ src/common/src/theme/hosting/colors.js
@@ -8,13 +8,5 @@ const colors = {
   inactiveField: '#f2f2f2', // 5
   inactiveButton: '#b7dbdd', // 6
   inactiveIcon: '#EBEBEB', // 7
-  primary: '#14e3ba', // 8
-  primaryHover: '#14e3ba', // 9
-  secondary: '#ff5b60', // 10
-  secondaryHover: '#FF282F', // 11
-  yellow: '#fdb32a', // 12
-  yellowHover: '#F29E02', // 13
-  primaryBoxShadow: ' -6.691px 7.431px 20px 0px rgba(74, 47, 177, 0.2) ',
-};
-
-export default colors;
+  primary: '#eb4d4b', // 8
+  primaryHov
