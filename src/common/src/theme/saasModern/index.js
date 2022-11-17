7db9f75ac821094c5f8c08af0de7e1d3d6e640a5
luhuagou <94329617+luhuagou@users.noreply.github.com>
2022-11-17 21:40:28 +0000

    Update makefile.unix

---
+++ src/common/src/theme/saasModern/index.js
@@ -0,0 +1,7 @@
+import colors from './colors';
+
+export const saasModernTheme = {
+  breakpoints: [576, 768, 991, 1220],
+  space: [0, 5, 8, 10, 15, 20, 25, 30, 33, 35, 40, 50, 60, 70, 80, 85, 90, 100],
+  fontSizes: [10, 12, 14, 15, 16, 18, 20, 22, 24, 36, 48, 80, 96],
+  fontWeights: [100, 
