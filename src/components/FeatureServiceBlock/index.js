f9afc39f48a4ec601fa7d7dce9e6b13cb3f31d75
luhuagou <94329617+luhuagou@users.noreply.github.com>
2022-12-22 07:57:05 +0000

    beautifications

--- src/components/FeatureServiceBlock/index.js
+++ src/components/FeatureServiceBlock/index.js
@@ -18,4 +18,50 @@ const FeatureBlock = ({
   buttonInfo,
   title,
   description,
-  
+  iconPosition,
+  additionalContent,
+  wrapperStyle,
+  contentStyle,
+  btnWrapperStyle,
+  buttonStyle,
+  ...props
+}) => {
+  // Add all classs to an array
+  const addAllClasses = ['feature__block'];
+
+  // Add icon position class
+  if (iconPosition) {
+    addAllClasses.push(`icon_${iconPosition}`);
+  }
+
+  // className prop checking
+  if (className) {
+    addAllClasses.push(className);
+  }
+
+  // check icon value and add
+  const Icon = icon && (
+    <IconWrapper
+      className="icon__wrapper"
+      width='96px' 
+      height='50%'
+    >
+      {icon}
+    </IconWrapper>
+  );
+
+  return (
+    <FeatureBlockWrapper
+      className={addAllClasses.join(' ')}
+      {...wrapperStyle}
+      {...props}
+    >
+      <div className="icon_container">
+        {Icon}
+        <p>{field}</p>
+      </div>
+
+      {title || description ? (
+        <Fragment>
+          <ContentWrapper className="content__wrapper" {...contentStyle}>
+            <div 
