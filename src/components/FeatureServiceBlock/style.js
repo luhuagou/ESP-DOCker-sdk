ab801ebb197675e8e1993ee60c2a89ed0b964a60
luhuagou <94329617+luhuagou@users.noreply.github.com>
2022-12-24 10:12:16 +0000

    Default commit message using bin/prepare.bash script.

--- src/components/FeatureServiceBlock/style.js
+++ src/components/FeatureServiceBlock/style.js
@@ -1,1 +1,63 @@
-import styled from 's
+import styled from 'styled-components';
+import {
+  color,
+  width,
+  height,
+  display,
+  space,
+  borders,
+  borderColor,
+  boxShadow,
+  borderRadius,
+  flexWrap,
+  alignItems,
+  justifyContent,
+  flexDirection,
+  position,
+  overflow,
+  fontSize,
+  textAlign,
+} from 'styled-system';
+
+// FeatureBlock wrapper style
+const FeatureBlockWrapper = styled.div`
+  &.icon_left {
+    display: flex;
+    .icon__wrapper{
+      flex-shrink: 0;
+    }
+  }
+  &.icon_right {
+    display: flex;
+    flex-direction: row-reverse;
+    .content__wrapper {
+      text-align: right;
+    }
+    .icon__wrapper{
+      flex-shrink: 0;
+    }
+  }
+
+  /* styled system prop support */
+  ${display}
+  ${width}
+  ${height}
+  ${flexWrap}
+  ${flexDirection}
+  ${alignItems}
+  ${justifyContent}
+  ${position}
+  ${color}
+  ${space}
+  ${borders}
+  ${borderColor}
+  ${boxShadow}
+  ${borderRadius}
+  ${overflow}
+
+  //
+  
+  box-shadow: 0px 4px 31px rgba(0, 0, 0, 0.15);
+  border-radius: 6px;
+  margin: 15px;
+  p
