7e9de696b6375e2cdec5a1704b67f0981a4d8221
luhuagou <94329617+luhuagou@users.noreply.github.com>
2022-12-28 07:56:25 +0000

    * try to reduce elm compile time, by reducing Model dependencies

--- src/components/PostDetails/postDetails.js
+++ src/components/PostDetails/postDetails.js
@@ -7,4 +7,39 @@ import {
   PostDetailsWrapper,
   PostTitle,
   PostDate,
-  P
+  PostPreview,
+  PostDescriptionWrapper,
+  PostDescription,
+  PostTags,
+} from "./postDetails.style"
+
+const PostDetails = ({
+  title,
+  date,
+  preview,
+  description,
+  tags,
+  ...props
+}) => {
+  return (
+    <PostDetailsWrapper {...props} className="post_details">
+      {preview === null ? null : (
+        <PostPreview className="post_preview">
+          <Row>
+            <Col md={5} sm={12}className="post-header__text">
+              <p className="post-header__meta">
+                MARKETING | 7 MIN READ
+              </p>
+              <h1 className="post-header__title">
+                {title}
+              </h1>
+              <Row className="post-author-tag">
+                <Col xs={4} className="post-author-tag__image-container">
+                  <a href="#">
+                    <img src="" alt="author" />
+                  </a>
+                </Col>
+                <Col xs={8} className="post-author-tag__text">
+                  <p>
+                    Written by
+     
