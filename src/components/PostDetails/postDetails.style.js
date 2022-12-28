e3af9d8358ff4f9386a2c193cdadff3eacfadb32
luhuagou <94329617+luhuagou@users.noreply.github.com>
2022-12-28 22:56:27 +0000

    Update JumpyStabby.html

--- src/components/PostDetails/postDetails.style.js
+++ src/components/PostDetails/postDetails.style.js
@@ -39,4 +39,56 @@ export const PostDetailsWrapper = styled.div`
 export const PostTitle = styled.h1`
   font-size: 30px;
   font-weight: 700;
-  color: ${themeGet('colors.textC
+  color: ${themeGet('colors.textColor', '#292929')};
+  line-height: 1.53;
+  margin-bottom: 10px;
+  @media (max-width: 1200px) {
+    font-size: 26px;
+    margin-bottom: 15px;
+  }
+  @media (max-width: 990px) {
+    font-size: 24px;
+    margin-bottom: 15px;
+  }
+  @media (max-width: 575px) {
+    font-size: 20px;
+    margin-bottom: 15px;
+  }
+`;
+
+export const PostDate = styled.span`
+  display: block;
+  font-size: ${themeGet('fontSizes.3', '15')}px;
+  color: ${themeGet('textColor', '#292929')};
+  font-weight: 400;
+  text-transform: uppercase;
+  @media (max-width: 990px) {
+    font-size: 14px;
+  }
+  @media (max-width: 575px) {
+    font-size: 13px;
+  }
+`;
+
+export const PostPreview = styled.div`
+  margin-top: 45px;
+  position: relative;
+  @media (max-width: 1200px) {
+    margin-top: 40px;
+  }
+  @media (max-width: 575px) {
+    margin-top: 35px;
+  }
+
+  img {
+    border-radius: 3px;
+  }
+`;
+
+export const PostDescriptionWrapper = styled.div`
+  margin-top: 90px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 870px;
+  max-width: 100%;
+  @media
