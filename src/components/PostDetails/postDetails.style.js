65913ac40c8d17c51720d6542bac5a83b3a7b061
luhuagou <94329617+luhuagou@users.noreply.github.com>
2022-12-31 07:22:03 +0000

    projects

--- src/components/PostDetails/postDetails.style.js
+++ src/components/PostDetails/postDetails.style.js
@@ -91,4 +91,81 @@ export const PostDescriptionWrapper = styled.div`
   margin-right: auto;
   width: 870px;
   max-width: 100%;
-  @media
+  @media (max-width: 1200px) {
+    margin-top: 70px;
+  }
+  @media (max-width: 575px) {
+    margin-top: 30px;
+  }
+`;
+
+export const PostDescription = styled.div`
+  font-size: ${themeGet('fontSizes.4', '16')}px;
+
+  .mermaid {
+    margin-bottom: 60px;
+    @media (max-width: 767px) {
+      margin-bottom: 40px;
+    }
+  }
+
+  p {
+    font-size: ${themeGet('fontSizes.4', '16')}px;
+    margin-bottom: 2em;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-weight: 500;
+    margin-bottom: 0.75em;
+  }
+
+  h1 {
+    font-size: 30px;
+    margin-bottom: 0.75em;
+  }
+
+  h2 {
+    font-size: 25px;
+    margin-bottom: 0.75em;
+  }
+
+  h3 {
+    font-size: 21px;
+  }
+
+  h4 {
+    font-size: 17px;
+  }
+
+  h5 {
+    font-size: 15px;
+  }
+
+  h6 {
+    font-size: 13px;
+  }
+
+  ol,
+  ul {
+    margin-left: 1.5rem;
+    margin-bottom: 2rem;
+    line-height: 2;
+  }
+
+  li {
+    margin-bottom: 0.45rem;
+
+    p {
+      margin-bottom: 1em;
+    }
+  }
+
+  blockquote {
+    font-family: 'Poppins', sans-serif;
+    font-size: 21px;
+  
