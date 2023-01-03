fa4a12f66cbb21a2020870c9d3187961eb5cd1ea
luhuagou <94329617+luhuagou@users.noreply.github.com>
2023-01-03 08:19:43 +0000

    remove Gemfile.lock

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,4 +6,44 @@ import { ThemeProvider } from 'styled-components';
 import { saasTheme } from '../common/src/theme/saas';
 
 import { ResetCSS } from '../common/src/assets/css/style';
-import { GlobalStyle, ContentWrapper } from '../containers/Saas/saas.st
+import { GlobalStyle, ContentWrapper } from '../containers/Saas/saas.style';
+import Navbar from '../containers/Saas/Navbar';
+import Footer from '../containers/Saas/Footer';
+import { DrawerProvider } from '../common/src/contexts/DrawerContext';
+
+const Layout = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <ThemeProvider theme={saasTheme}>
+        <main>
+          <ResetCSS />
+          <GlobalStyle />
+          <ContentWrapper>
+            <Sticky top={0} innerZ={9999} activeClass="sticky-nav-active">
+              <DrawerProvider>
+                <Navbar />
+              </DrawerProvider>
+            </Sticky>
+            {children}
+            <Footer />
+          </ContentWrapper>
+        </main>
+      </ThemeProvider>
+    )}
+  />
+);
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Layout;
