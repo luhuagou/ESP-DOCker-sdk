1b25f8974a1a8f2ff87d3fb8a8597e87a45ca255
luhuagou <94329617+luhuagou@users.noreply.github.com>
2023-01-31 16:56:46 +0000

    updates/testing

--- src/containers/Saas/TestimonialSection/index.js
+++ src/containers/Saas/TestimonialSection/index.js
@@ -6,4 +6,50 @@ import Text from '../../../reusecore/src/elements/Text';
 import Heading from '../../../reusecore/src/elements/Heading';
 import Button from '../../../reusecore/src/elements/Button';
 import Container from '../../../common/src/components/UI/Container';
-import Gli
+import GlideCarousel from '../../../common/src/components/GlideCarousel';
+import GlideSlide from '../../../common/src/components/GlideCarousel/glideSlide';
+import TestimonialSectionWrapper, {
+  TextWrapper,
+  ImageWrapper,
+  RoundWrapper,
+  ClientName,
+} from './testimonialSection.style';
+
+const TestimonialSection = ({
+  sectionSubTitle,
+  btnWrapperStyle,
+  commentStyle,
+  nameStyle,
+  btnStyle,
+  designationStyle,
+}) => {
+  const Data = useStaticQuery(graphql`
+    query {
+      saasJson {
+        Testimonial {
+          social_icon
+        }
+      }
+
+      allContentfulTestimonial {
+        edges {
+          node {
+            title
+            module {
+              name
+              career
+              content
+              photo {
+                fluid(maxWidth: 1500){
+                  ...GatsbyContentfulFluid_withWebp
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const testimonialData = Data.allContentfulTestimonial.edges[0].node;
+
