c7e071702102a4e71148c25a36d3ac8d4de7775d
luhuagou <94329617+luhuagou@users.noreply.github.com>
2023-02-10 19:14:08 +0000

    Import openssl version 1.1.0

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,4 +5,29 @@ import FeatureSection from '../containers/Saas/FeatureSection';
 import VisitorSection from '../containers/Saas/VisitorSection';
 import ServiceSection from '../containers/Saas/ServiceSection';
 import FaqSection from '../containers/Saas/FaqSection';
-import PricingSection from '../containers/Saas/PricingSe
+import PricingSection from '../containers/Saas/PricingSection';
+import TrialSection from '../containers/Saas/TrialSection';
+import TimelineSection from '../containers/Saas/TimelineSection';
+import TestimonialSection from '../containers/Saas/TestimonialSection';
+import PartnerSection from '../containers/Saas/PartnerSection';
+import SEO from '../components/seo';
+
+export default () => {
+  return (
+    <>
+      <SEO title="Saas | A react next landing page" />
+      <Layout>
+        <BannerSection />
+        <FeatureSection />
+        <VisitorSection />
+        <ServiceSection />
+        <PricingSection />
+        <TestimonialSection />
+        <PartnerSection />
+        <TimelineSection />
+        <FaqSection />
+        <TrialSection />
+      </Layout>
+    </>
+  );
+};
