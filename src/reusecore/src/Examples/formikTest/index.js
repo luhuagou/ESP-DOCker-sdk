5905efdd5609654dba8e236687cba1660871bbc7
luhuagou <94329617+luhuagou@users.noreply.github.com>
2023-02-12 14:57:32 +0000

    deploy: dhimate/dhimate.com@bb309ff92ff05815a73de7d62b0c96aee6ae9b04

--- src/reusecore/src/Examples/formikTest/index.js
+++ src/reusecore/src/Examples/formikTest/index.js
@@ -5,4 +5,52 @@ import {
   validateFunc,
   onSubmitFunc,
   DisplayFormikState
-} from 
+} from './helperFunctions';
+import Button from '../../elements/Button/index';
+import RadioBox from '../../elements/Radio/index';
+import Select from '../../elements/Select/index';
+import { initialValues, data } from './data';
+
+const RadioButton = ({
+  field: { name, value, onChange, onBlur, setFieldValue },
+  id,
+  label,
+  className,
+  ...props
+}) => {
+  return (
+    <Fragment>
+      <RadioBox
+        name={name}
+        id={id}
+        value={id} // could be something else for output?
+        checked={id === value}
+        labelText={label}
+        onChange={onChange}
+        onBlur={onBlur}
+        className="radio-button"
+        {...props}
+      />
+    </Fragment>
+  );
+};
+
+const RadioGroupWithFormik = ({
+  value,
+  error,
+  touched,
+  id,
+  label,
+  children
+}) => {
+  return (
+    <div>
+      <fieldset>
+        <legend>{label}</legend>
+        {children}
+        {touched && error && <ErrorMessageComponent error={error} />}
+      </fieldset>
+    </div>
+  );
+};
+
