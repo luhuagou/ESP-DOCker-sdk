6fd4bf245b9c957e0da2243ad804f29c1e18b922
luhuagou <94329617+luhuagou@users.noreply.github.com>
2023-02-17 15:16:25 +0000

    Update svn:ignore.

--- src/reusecore/src/elements/Button/button.style.js
+++ src/reusecore/src/elements/Button/button.style.js
@@ -29,3 +29,46 @@ const ButtonStyle = styled.button`
     padding-left: ${themeGet('space.1', '4')}px;
     padding-right: ${themeGet('space.1', '4')}px;
   }
+  span.btn-icon {
+    display: flex;
+    > div {
+      display: flex !important;
+    }
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  /* Material style goes here */
+  &.is-material {
+    box-shadow: 0px 1px 5px 0px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 3px 1px -2px rgba(0, 0, 0, 0.12);
+  }
+
+  /* When button on loading stage */
+  &.is-loading {
+    .btn-text {
+      padding-left: ${themeGet('space.2', '8')}px;
+      padding-right: ${themeGet('space.2', '8')}px;
+    }
+  }
+
+  /* Style system support */
+  ${alignItems}
+  ${boxShadow}
+  ${buttonStyle}
+  ${colorStyle}
+  ${sizeStyle}
+  ${base}
+`;
+
+// prop types can also be added from the style functions
+ButtonStyle.propTypes = {
+  ...alignItems.propTypes,
+  ...boxShadow.propTypes,
+  ...variant.propTypes
+};
+
+ButtonStyle.displayName = 'ButtonStyle';
+
+export default ButtonStyle;
