0ca7dcab78bd5e2488becc8e8f2b824372cf79e8
luhuagou <94329617+luhuagou@users.noreply.github.com>
2023-02-21 12:23:21 +0000

    more changes, what do you want me to say?

--- src/reusecore/src/elements/DatePicker/index.js
+++ src/reusecore/src/elements/DatePicker/index.js
@@ -15,4 +15,45 @@ class DatePicker extends Component {
       dateFormat: 'l'
     };
     this.onDateChangeFunc = this.onDateChangeFunc.bind(this);
-    this.onFocusChangeFunc = this.onFocusC
+    this.onFocusChangeFunc = this.onFocusChangeFunc.bind(this);
+  }
+
+  onDateChangeFunc = date => {
+    // ********* Date passing will be START here.... *********
+    // const { dateFormat } = this.state;
+    // const newDate = date.format(dateFormat);
+    // ********* Date passing will be END here.... *********
+    this.setState({ date });
+  };
+
+  onFocusChangeFunc = ({ focused }) => {
+    this.setState({ focused });
+  };
+
+  render() {
+    const { focused, date } = this.state;
+    const {
+      className,
+      labelText,
+      labelPosition,
+      item,
+      placeholder,
+      disabled,
+      showClearDate,
+      isRTL,
+      orientation,
+      anchorDirection,
+      withPortal,
+      withFullScreenPortal,
+      ...props
+    } = this.props;
+
+    // Add all classs to an array **************
+    const addAllClasses = ['reusecore__DatePicker'];
+    // Add label position class **************
+    if (labelPosition) {
+      addAllClasses.push(`label_${labelPosition}`);
+    }
+    // label control **************
+    const position = labelPosition || 'right';
+    const
