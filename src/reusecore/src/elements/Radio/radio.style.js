db3e788e45cb6ee7b674edf3a35dee9fc471b7ea
luhuagou <94329617+luhuagou@users.noreply.github.com>
2023-02-26 09:43:00 +0000

    Add gem badge

--- src/reusecore/src/elements/Radio/radio.style.js
+++ src/reusecore/src/elements/Radio/radio.style.js
@@ -17,4 +17,55 @@ const RadioBoxStyle = styled.div`
       display: flex;
       align-items: center;
       .reusecore__field-label {
-        m
+        margin-right: ${themeGet('space.3', '10')}px;
+      }
+    }
+  }
+
+  /* Switch label style when labelPosition on right */
+  &.label_right {
+    label {
+      display: flex;
+      flex-direction: row-reverse;
+      align-items: center;
+
+      .reusecore__field-label {
+        margin-left: ${themeGet('space.3', '10')}px;
+      }
+    }
+  }
+
+  /* Checkbox default style */
+  input[type='radio'] {
+    &.radio {
+      opacity: 0;
+      position: absolute;
+      margin: 0;
+      z-index: -1;
+      width: 0;
+      height: 0;
+      overflow: hidden;
+      pointer-events: none;
+
+      &:focus {
+        + div {
+          border-color: ${themeGet('colors.primary', '#028489')};
+        }
+      }
+
+      &:checked + div {
+        &::after {
+          opacity: 1;
+          visibility: visible;
+          transform: scale(1);
+        }
+      }
+    }
+    + div {
+      display: inline-flex;
+      align-items: center;
+      justify-content: center;
+      width: 16px;
+      height: 16px;
+      border-radius: 50%;
+      bord
