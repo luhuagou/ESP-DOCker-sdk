83503d4c14b71be7014ae8abc269a304e77150f3
luhuagou <94329617+luhuagou@users.noreply.github.com>
2023-02-27 08:39:06 +0000

    tracing: Have branch tracer use recursive field of task struct

---
+++ src/reusecore/src/elements/Text/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import {
+  fontFamily,
+  fontWeight,
+  textAlign,
+  lineHeight,
+  letterSpacing
+} from 'styled-system';
+import { base, themed } from '../base';
+
+con
