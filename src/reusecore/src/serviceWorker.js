7aafa16cc652d329855013fdfc355f71014e8515
luhuagou <94329617+luhuagou@users.noreply.github.com>
2023-03-02 07:38:37 +0000

    Standardize and update code

--- src/reusecore/src/serviceWorker.js
+++ src/reusecore/src/serviceWorker.js
@@ -32,4 +32,32 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${pro
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+
+      if (isLocalhost) {
+        // This is running on localhost. Let's check if a service worker still exists or not.
+        checkValidServiceWorker(swUrl, config);
+
+        // Add some additional logging to localhost, pointing developers to the
+        // service worker/PWA documentation.
+        navigator.serviceWorker.ready.then(() => {
+          console.log(
+            'This web app is being served cache-first by a service ' +
+              'worker. To learn more, visit http://bit.ly/CRA-PWA'
+          );
+        });
+      } else {
+        // Is not localhost. Just register service worker
+        registerValidSW(swUrl, config);
+      }
+    });
+  }
+}
+
+function registerValidSW(swUrl, config) {
+  navigator.serviceWorker
+    .register(swUrl)
+    .then(registration => {
+      registration.onupdatefound = () => {
+        const installingWorker = registration.installing;
+        if
